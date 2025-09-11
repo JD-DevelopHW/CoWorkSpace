6557f4598a12feb4a1004d86413bbffbe9a27037
juan david <juandaob21@gmail.com>
2025-09-11 11:47:44 -0500

    Dtos de creacion de usuarios

---
+++ Cwk.Domain/DTOs/Responses/UserResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using Cwk.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwk.Domain.DTOs.Responses
+{
+    public class UserResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public string? PhoneNumber { get; set; }
+        public Role Role { get; set; }
+    }
+}
