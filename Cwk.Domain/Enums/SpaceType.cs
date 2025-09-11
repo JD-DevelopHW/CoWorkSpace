56e8b721ad83965ec876c49f637f84c35c270ad3
juan david <juandaob21@gmail.com>
2025-09-11 11:22:12 -0500

    migraciones de la base de datos y entidades

---
+++ Cwk.Domain/Enums/SpaceType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwk.Domain.Enums
+{
+    public enum SpaceType
+    {
+        Office,
+        MeetingRoom,
+        CoworkingSpace,
+        EventSpace,
+        PrivateOffice,
+        storage,
+        kitchen,
+        individualDesk
+    }
+}
---
+++ Cwk.Infraestruture/Data/AppDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwk.Infraestruture.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CWKDB;Trusted_Connection=true;MultipleActiveResultSets=true");
+            return new AppDbContext(optionBuilder.Options);
+        }
+    }
+}
