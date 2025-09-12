0dc4e280e6c1c5814293ac7518ad3d895e4607ed
juan david <juandaob21@gmail.com>
2025-09-11 23:24:24 -0500

    configuracion del token para autenticación del usuario

---
+++ Cwk.Application/Interfaces/ITokenServices.cs
@@ -0,0 +1,9 @@
+﻿using Cwk.Domain.Entities;
+
+namespace Cwk.Application.Interfaces
+{
+    public interface ITokenServices
+    {
+        public string GenerateToken(User user);
+    }
+}
---
+++ Cwk.Application/Services/TokenServices.cs
@@ -0,0 +1,21 @@
+﻿using Cwk.Application.Interfaces;
+using Cwk.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwk.Application.Services
+{
+    public class TokenServices(IConfiguration config) : ITokenServices
+    {
+        private readonly string secretKey = config.GetSection("Jwt").GetValue<string>("key");
+
+        public string GenerateToken(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
